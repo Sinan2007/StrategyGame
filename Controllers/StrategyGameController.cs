6d3a8fc6e6a4174cfd003478fc3e788a1827764d
Sinan2007 <sinancetalak@gmail.com>
2025-04-22 10:03:25 +0300

    Add files via upload

---
+++ Controllers/StrategyGameController.cs
@@ -0,0 +1,28 @@
+﻿using Strategy_Game.Data.Models;
+using Strategy_Game.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Strategy_Game.Controllers
+{
+    public class StrategyGameController
+    {
+        public StrategyGameContext context = new StrategyGameContext();
+        public List<Player> AllPlayers()
+        {
+            return context.Players.ToList();
+        }
+        public int ResultOfLast5Battles()
+        {
+            return context.BattleUnits.OrderByDescending(b => b.Id).Take(5).Sum(x => x.Quantity);
+        }
+        public List<Faction> BuildingUnits()
+        {
+            return context.Factions.Include(f => f.Buildings).Include(f => f.Units).Where(f => f.Name == "Humans").ToList();
+        }
+    }
+}
