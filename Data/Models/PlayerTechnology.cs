f954481c625bf1db152d8af3e169a96de207e6e3
Sinan2007 <sinancetalak@gmail.com>
2025-04-22 10:03:01 +0300

    Add files via upload

---
+++ Data/Models/PlayerTechnology.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy_Game.Data.Models;
+
+public partial class PlayerTechnology
+{
+    public int Id { get; set; }
+
+    public int? PlayerId { get; set; }
+
+    public int? TechnologyId { get; set; }
+
+    public string? IsResearched { get; set; }
+
+    public DateOnly? ResearchedAt { get; set; }
+
+    public virtual Player? Player { get; set; }
+
+    public virtual Technology? Technology { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Strategy_Game.Views;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Display display = new Display();
+        display.ShowMenu();
+    }
+}
